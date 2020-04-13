252f4b9cbce1d23929035da67dee6fd7b0dd4176
JIHONG GIL <42768552+jhgil92@users.noreply.github.com>
2020-04-13 15:48:15 +0900

    Update event_path.sql

--- event_path.sql
+++ event_path.sql
@@ -18,7 +18,7 @@ FROM (
       event_name as EVENT_NAME,
       user_pseudo_id as FIREBASE_ID
     FROM
-      `neo-smart-gcm.analytics_196410282.events_*`,
+      `####.events_*`,
       UNNEST (event_params) as ep
     WHERE
       (event_name LIKE 'SCREEN%' OR event_name LIKE 'EVENT%' OR event_name = 'session_start') AND (ep.key = 'ga_session_id') AND _TABLE_SUFFIX BETWEEN '20200407' AND '20200407' # 조회기간 (시작일, 종료일)
@@ -27,7 +27,7 @@ FROM (
     )
   ORDER BY 6, 4, 2
 ) a
-LEFT JOIN `neo-smart-gcm.analytics_196410282.event_name_list` AS b1 ON a.EVENT_NAME = b1.internal_event_name
-LEFT JOIN `neo-smart-gcm.analytics_196410282.event_name_list` AS b2 ON a.BF_EVENT = b2.internal_event_name
-LEFT JOIN `neo-smart-gcm.analytics_196410282.event_name_list` AS b3 ON a.AF_EVENT = b3.internal_event_name
+LEFT JOIN `####.event_name_list` AS b1 ON a.EVENT_NAME = b1.internal_event_name
+LEFT JOIN `####.event_name_list` AS b2 ON a.BF_EVENT = b2.internal_event_name
+LEFT JOIN `####.event_name_list` AS b3 ON a.AF_EVENT = b3.internal_event_name
 ORDER BY 6, 4, 2
